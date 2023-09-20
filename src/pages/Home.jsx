bf0e82ea1cf2ffb8d4c69492500219a6afdd1151
kiisi <kiisifelix06@gmail.com>
2023-09-20 23:37:51 +0100

    [feat] Home hero page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,36 @@ export default function Home() {
                 <div className="border-b-[1px] border-b-[#FFFFFF2E]">
                     <Navbar className="pb-5" />
                 </div>
-                <Container className="!px-4 flex justify-end">
-                    <h1 className="font-bold italic py-6 lg:text-[36px] text-white">Igniting a Revolution in <span className="relative">HR Innovation <img src="/images/curved-line.svg" className="bottom-[-10px] lg:bottom-[-15px] left-[2px] absolute"/></span></h1>
-                </Container>
+                <Hero />
             </div>
+        </>
+    )
+}
+
+const Hero = () => {
+
+    return (
+        <>
+            <Container className="!px-4 flex justify-end relative">
+                <img src="/images/purple-lens-flare-left.svg" alt="purple lens flare" className="absolute pointer-events-none top-[-300px] left-[-100px] mix-blend-hard-light" />
+                <h1 className="font-bold italic py-6 lg:text-[36px] text-white">Igniting a Revolution in <span className="relative">HR Innovation <img src="/images/curved-line.svg" className="bottom-[-10px] lg:bottom-[-15px] left-[2px] absolute" /></span></h1>
+
+            </Container>
+            <Container className="grid lg:grid-cols-2 pt-10">
+                <div>
+
+                </div>
+                <figure>
+                    {/* <img 
+                        src="" 
+                        alt=""
+                    /> */}
+                    <img 
+                        src="/images/man-vr.svg" 
+                        className="w-full h-full" 
+                    />
+                </figure>
+            </Container>
         </>
     )
 }

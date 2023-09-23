1f495c73bd7c590ba898ee9138fd8f32fb8b8691
kiisi <kiisifelix06@gmail.com>
2023-09-23 14:23:19 +0100

    [feat] homepage introduction section star gazers and arrows implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -56,9 +56,9 @@ const Hero = () => {
                         <div className="grid place-items-center text-center lg:text-left lg:block font-clashdisplay text-white text-[32px] lg:text-[80px] font-bold lg:leading-[98px] relative z-2">
                             <img src="/icons/star-white.svg" alt="Star White" className="absolute w-[10px] h-[12px] lg:w-[26px] lg:h-[32px] top-[-20px] left-[70px] lg:left-[50px] lg:top-[-120px] pointer-events-none" />
                             <h1>getlinked Te
-                                <span className="inline-block relative">c <img src="/icons/light-bulb.svg" alt="light bulb" className="absolute left-[5px] lg:left-[15px] top-[-10px] lg:top-[-42px] w-[18px] h-[26px] lg:h-[73px] lg:w-[53px]" /> 
+                                <span className="inline-block relative">c <img src="/icons/light-bulb.svg" alt="light bulb" className="absolute left-[5px] lg:left-[15px] top-[-10px] lg:top-[-42px] w-[18px] h-[26px] lg:h-[73px] lg:w-[53px]" />
                                 </span>
-                            h</h1>
+                                h</h1>
                             <h1>Hackathon&nbsp;
                                 <span className="text-primary inline-block relative">1.0
                                     <img src="/icons/chain.svg" alt="chain" className="inline-block mb-2 lg:mb-0 lg:absolute w-[32px] h-[32px] lg:w-auto lg:h-auto top-[5px] left-[120px]" />
@@ -110,14 +110,17 @@ const BigIdea = () => {
     return (
         <Container className="pt-[62px] pb-[78px]">
             <div className="grid lg:grid-cols-2 gap-[109px] max-w-max mx-auto">
-                <figure>
+                <figure className="relative">
                     <img src="/images/bigidea.svg" alt="Big Idea" />
+                    <img src="/icons/arrow.svg" alt="Arrow" className="absolute w-[19px] h-[22px] lg:w-[49px] lg:h-[57px] bottom-[-15px] translate-x-[-50%] right-[50%] lg:right-[0px] lg:bottom-[0px] pointer-events-none" />
+                    <img src="/icons/star-dark-purple.svg" alt="Star Purple" className="absolute pointer-events-none w-[8px] h-[10px] lg:w-[21px] lg:h-[25px] left-[-20px] top-[35%] translate-y-[-50%]" />
                 </figure>
                 <div className="flex items-center">
                     <div className="w-full max-w-[535px] grid place-items-center lg:block text-center lg:text-left">
-                        <header className="text-white text-[32px] font-clashdisplay font-bold">
+                        <header className="text-white text-[32px] font-clashdisplay font-bold relative">
                             <h1>Introduction to getlinked</h1>
                             <h1 className="text-primary">tech Hackathon 1.0</h1>
+                            <img src="/icons/star-purple.svg" alt="Star Purple" className="absolute pointer-events-none w-[8px] h-[10px] lg:w-[30px] lg:h-[36px] right-0 top-[50%] translate-y-[-50%]" />
                         </header>
                         <div className="pt-2">
                             <p className="text-white text-[14px] leading-[27px]">Our tech hackathon is a melting pot of visionaries, and its purpose is as

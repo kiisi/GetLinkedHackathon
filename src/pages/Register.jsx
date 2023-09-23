a092f8bf5671266a09c2cd14fc51865dccdc7c91
kiisi <kiisifelix06@gmail.com>
2023-09-23 11:08:33 +0100

    [fix] checkbox background color

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -385,7 +385,7 @@ const Checkbox = ({ checked, setChecked }) => {
     }
 
     return (
-        <div className="w-[14px] h-[14px] shrink-0 mt-[5px] self-start rounded-[2px] border-[1px] border-white" onClick={checkedHandler}>
+        <div className={`w-[14px] h-[14px] shrink-0 mt-[5px] self-start rounded-[2px] border-[1px] border-white ${checked ? "close-btn-gradient" : ""}`} onClick={checkedHandler}>
             {
                 checked && (
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="text-white"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
